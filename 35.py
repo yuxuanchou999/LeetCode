e812626c03c65e25b7fffaac66852cbd21af79a5
YUXUAN ZHOU <yuxuanchou999@gmail.com>
2018-05-28 22:01:22 +0800

    0528

---
+++ 35.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            if nums[i] > target:
+                return i
+        return len(nums)
