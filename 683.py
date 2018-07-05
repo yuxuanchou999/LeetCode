49b2fc7db0ef29f4f0cbee81f42b56f33517813e
YUXUAN ZHOU <yuxuanchou999@gmail.com>
2018-07-05 10:04:43 +0800

    0705

---
+++ 683.py
@@ -0,0 +1,20 @@
+class Solution:
+    def kEmptySlots(self, flowers, k):
+        """
+        :type flowers: List[int]
+        :type k: int
+        :rtype: int
+        """
+        active = []
+        for day, flower in enumerate(flowers, 1):
+            i = bisect.bisect(active, flower)
+            for neighbor in active[i-(i>0):i+1]:
+                if abs(neighbor - flower) - 1 == k:
+                    return day
+            active.insert(i, flower)
+        return -1
+        
+        
+            
+            
+            
