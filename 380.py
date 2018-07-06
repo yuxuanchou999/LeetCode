37c9f09bddd512483ff0022cd24b425c8149c19a
YUXUAN ZHOU <yuxuanchou999@gmail.com>
2018-07-06 20:00:14 +0800

    0706

---
+++ 380.py
@@ -0,0 +1,46 @@
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums, self.pos = [], {}
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.pos: return False
+        self.nums.append(val)
+        self.pos[val] = len(self.nums) - 1
+        return True
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.pos: return False
+        idx, last = self.pos[val], self.nums[-1]
+        self.nums[idx], self.pos[last] = last, idx
+        self.nums.pop()
+        self.pos.pop(val, 0)
+        return True
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
