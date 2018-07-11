41003815dcaf44c1a6dabf5f5fb0982fff746bfc
YUXUAN ZHOU <yuxuanchou999@gmail.com>
2018-07-11 09:57:51 +0800

    0711

---
+++ 327.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countRangeSum(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: int
+        """
+        def sort(lo, hi):
+            mid = (lo + hi) // 2
+            if mid == lo: return 0
+            count = sort(lo, mid) + sort(mid, hi)
+            i = j = mid
+            for left in first[lo:mid]:
+                while i < hi and first[i] - left < lower: i += 1
+                while j < hi and first[j] - left <= upper: j += 1
+                count += j - i
+            first[lo:hi] = sorted(first[lo:hi])
+            return count
+            
+        first = [0]
+        for num in nums:
+            first.append(first[-1] + num)
+        return sort(0, len(first))
+        
