1995fbf1a4c4d19aa7345b3392c24b99f4489291
YUXUAN ZHOU <yuxuanchou999@gmail.com>
2018-11-24 23:26:56 -0800

    1124

---
+++ 772.py
@@ -0,0 +1,41 @@
+class Solution:
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        operands, operators = [], []
+        operand = ''
+        for i in reversed(range(len(s))):
+            if s[i].isdigit():
+                operand += s[i]
+                if i == 0 or not s[i - 1].isdigit():
+                    operands.append(int(operand[::-1]))
+                    operand = ''
+            elif s[i] in '*/)':
+                operators.append(s[i])
+            elif s[i] in '+-':
+                while operators and operators[-1] in '/*':
+                    self.compute(operands, operators)
+                operators.append(s[i])
+            elif s[i] == '(':
+                while operators[-1] != ')':
+                    self.compute(operands, operators)
+                operators.pop()
+        
+        while operators:
+            self.compute(operands, operators)
+            
+        return operands[-1]
+    
+    def compute(self, operands, operators):
+        left, right = operands.pop(), operands.pop()
+        op = operators.pop()
+        if op == '+':
+            operands.append(left + right)
+        elif op == '-':
+            operands.append(left - right)
+        elif op == '*':
+            operands.append(left * right)
+        elif op == '/':
+            operands.append(int(left / right))
